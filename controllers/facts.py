0b8121e2bd42ed1489cb79db79b08be45c5cb681
root <root@x200s.(none)>
2009-10-09 11:32:00 +0200

    little content-changes in facts

--- controllers/facts.py
+++ controllers/facts.py
@@ -9,6 +9,10 @@ log = logging.getLogger(__name__)
 class FactsController(BaseController):
 
     def index(self):
+        time_format = "%Y-%m-%d"
+        birthday = datetime.datetime(*time.strptime('1980-10-02',time_format)[:6])
+        now=datetime.datetime.now()
+        c.age=(now-birthday).days/365
         return render("/facts/index.html")
  
     def stats(self):
