b7a9ee0d1beb8bfb21499edbd7b8da87cc80d8ea
root <root@benko.login.cx>
2009-10-20 19:19:33 +0000

    controller/track: display infomarker-icons only when there's related data(log, gallery) available ajax and infomarker-icon-links are generated in the controller now(helps to display stuff only when available) templates/track/site.html: changed the respective js-functions to work with new ajax-strings("subcontent" instead of "gallery")

--- controllers/track.py
+++ controllers/track.py
@@ -36,14 +36,31 @@ class TrackController(BaseController):
             q = model.Session.query(model.trackpoint).filter(model.trackpoint.infomarker==True)
             c.infomarkers = q.order_by(asc(model.trackpoint.timestamp)).all()
         for c.infomarker in c.infomarkers:
+            q = model.Session.query(model.imageinfo).filter(model.imageinfo.infomarker_id==c.infomarker.id)
+            if q.count() > 0:
+                firstimage = q.order_by(asc(model.imageinfo.id)).limit(1)
+                for image in firstimage:
+                    #creates the infomarker-image_icon-and-ajax-link(fancy escaping for js needed):
+                    gallerylink="""<span class=\\"image_icon\\"><a href=\\"javascript:showSubcontent(\'/track/gallery/%s/%s\')\\"></a></span>""" % (c.infomarker.id,image.id)
+            else:
+                gallerylink=''
+            q = model.Session.query(model.log).filter(model.log.infomarker_id==c.infomarker.id)
+            if q.count() > 0:
+                firstlog = q.order_by(asc(model.log.id)).limit(1)
+                for log in firstlog:
+                    #creates the infomarker-log_icon-and-ajax-link(fancy escaping for js needed):
+                    loglink="""<span class=\\"log_icon\\"><a href=\\"javascript:showSubcontent(\'/track/log/%s/%s\')\\"></a></span>""" % (c.infomarker.id,log.id)
+            else:
+                loglink=''
+
             q = model.Session.query(model.track).filter(model.track.id==c.infomarker.track_id)
             if q.count() == 1:
                 c.track = q.one()
                 total_mins = c.track.timespan.seconds / 60
                 mins = total_mins % 60
                 hours = total_mins / 60
-                timespan = str(hours)+'h '+str(mins)+'min'
-                rounded_distance=str(c.track.distance.quantize(Decimal("0.01"), ROUND_HALF_UP))+'km'
+                timespan = '<b>duration:</b> %sh%smin<br />' % (str(hours),str(mins))
+                rounded_distance='<b>distance:</b> %skm<br />' % (str(c.track.distance.quantize(Decimal("0.01"), ROUND_HALF_UP)))
                 date=c.track.date.strftime('%B %d, %Y')
                 trackpts=c.track.gencpoly_pts
                 tracklevels=c.track.gencpoly_levels
@@ -52,15 +69,63 @@ class TrackController(BaseController):
                 q = model.Session.query(model.timezone).filter(model.timezone.id==c.infomarker.timezone_id)
                 c.timezone = q.one()
                 localtime=c.infomarker.timestamp+c.timezone.utcoffset
+                date=localtime.strftime('%B %d, %Y')
                 rounded_distance=''
                 timespan=''
-                date=localtime.strftime('%B %d, %Y')
                 trackpts=''
                 tracklevels=''
                 trackcolor=''
-            c.markerlist=c.markerlist + '''{'lat':%s, 'lon':%s, 'gal':"/track/gallery/%s/0", 'trackdate':"%s", 'distance':"%s", 'timespan':"%s", 'encpts':"%s", 'enclvl':"%s", 'color':"%s"},''' % (c.infomarker.latitude,c.infomarker.longitude,c.infomarker.id,date,rounded_distance,timespan,trackpts,tracklevels,trackcolor)
+            c.markerlist=c.markerlist + '''{'lat':%s, 'lon':%s, 'gal':"%s",'log':"%s", 'markerdate':"%s", 'distance':"%s", 'timespan':"%s", 'encpts':"%s", 'enclvl':"%s", 'color':"%s"},''' % (c.infomarker.latitude,c.infomarker.longitude,gallerylink,loglink,date,rounded_distance,timespan,trackpts,tracklevels,trackcolor)
         c.markerlist=c.markerlist + '''];'''
         return render("/track/index.html")
+
+    def infomarker(self,id):
+        c.markerlist='''['''
+        q = model.Session.query(model.trackpoint).filter(model.trackpoint.id==id)
+        c.infomarker = q.one()
+        q = model.Session.query(model.imageinfo).filter(model.imageinfo.infomarker_id==c.infomarker.id)
+        if q.count() > 0:
+            firstimage = q.order_by(asc(model.imageinfo.id)).limit(1)
+            for image in firstimage:
+                #creates the infomarker-image_icon-and-ajax-link(fancy escaping for js needed):
+                gallerylink="""<span class=\\"image_icon\\"><a href=\\"javascript:showSubcontent(\'/track/gallery/%s/%s\')\\"></a></span>""" % (c.infomarker.id,image.id)
+        else:
+            gallerylink=''
+        q = model.Session.query(model.log).filter(model.log.infomarker_id==c.infomarker.id)
+        if q.count() > 0:
+            firstlog = q.order_by(asc(model.log.id)).limit(1)
+            for log in firstlog:
+                #creates the infomarker-log_icon-and-ajax-link(fancy escaping for js needed):
+                loglink="""<span class=\\"log_icon\\"><a href=\\"javascript:showSubcontent(\'/track/log/%s/%s\')\\"></a></span>""" % (c.infomarker.id,log.id)
+        else:
+            loglink=''
+        q = model.Session.query(model.track).filter(model.track.id==c.infomarker.track_id)
+        if q.count() == 1:
+            c.track = q.one()
+            total_mins = c.track.timespan.seconds / 60
+            mins = total_mins % 60
+            hours = total_mins / 60
+            timespan = '<b>duration:</b> %sh%smin<br />' % (str(hours),str(mins))
+            rounded_distance = '<b>distance:</b> %skm<br />' % (str(c.track.distance.quantize(Decimal("0.01"), ROUND_HALF_UP)))
+            date=c.track.date.strftime('%B %d, %Y')
+            trackpts=c.track.gencpoly_pts
+            tracklevels=c.track.gencpoly_levels
+            trackcolor=c.track.color
+        else:
+            q = model.Session.query(model.timezone).filter(model.timezone.id==c.infomarker.timezone_id)
+            c.timezone = q.one()
+            localtime=c.infomarker.timestamp+c.timezone.utcoffset
+            rounded_distance=''
+            timespan=''
+            date=localtime.strftime('%B %d, %Y')
+            trackpts=''
+            tracklevels=''
+            trackcolor=''
+        c.markerlist=c.markerlist + '''{'lat':%s, 'lon':%s, 'gal':"%s",'log':"%s", 'markerdate':"%s", 'distance':"%s", 'timespan':"%s", 'encpts':"%s", 'enclvl':"%s", 'color':"%s"},''' % (c.infomarker.latitude,c.infomarker.longitude,gallerylink,loglink,date,rounded_distance,timespan,trackpts,tracklevels,trackcolor)
+        c.markerlist=c.markerlist + '''];'''
+        return render("/track/index.html")
+
+
 
     def gallery(self,infomarker,startfromimg):
         #first imageid
@@ -100,33 +165,4 @@ class TrackController(BaseController):
                 c.prevstartfromimg=image.id
         return render("/track/gallery.html")
     
-    def infomarker(self,id):
-        c.markerlist='''['''
-        q = model.Session.query(model.trackpoint).filter(model.trackpoint.id==id)
-        c.infomarker = q.one()
-        q = model.Session.query(model.track).filter(model.track.id==c.infomarker.track_id)
-        if q.count() == 1:
-            c.track = q.one()
-            total_mins = c.track.timespan.seconds / 60
-            mins = total_mins % 60
-            hours = total_mins / 60
-            timespan = str(hours)+'h '+str(mins)+'min'
-            rounded_distance=str(c.track.distance.quantize(Decimal("0.01"), ROUND_HALF_UP))+'km'
-            date=c.track.date.strftime('%B %d, %Y')
-            trackpts=c.track.gencpoly_pts
-            tracklevels=c.track.gencpoly_levels
-            trackcolor=c.track.color
-        else:
-            q = model.Session.query(model.timezone).filter(model.timezone.id==c.infomarker.timezone_id)
-            c.timezone = q.one()
-            localtime=c.infomarker.timestamp+c.timezone.utcoffset
-            rounded_distance=''
-            timespan=''
-            date=localtime.strftime('%B %d, %Y')
-            trackpts=''
-            tracklevels=''
-            trackcolor=''
-        c.markerlist=c.markerlist + '''{'lat':%s, 'lon':%s, 'gal':"/track/gallery/%s/0", 'trackdate':"%s", 'distance':"%s", 'timespan':"%s", 'encpts':"%s", 'enclvl':"%s", 'color':"%s"},''' % (c.infomarker.latitude,c.infomarker.longitude,c.infomarker.id,date,rounded_distance,timespan,trackpts,tracklevels,trackcolor)
-        c.markerlist=c.markerlist + '''];'''
-        return render("/track/index.html")
 
